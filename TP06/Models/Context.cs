2c92d6baad4969e002b2c8d06c04ce72e43f0aa5
Armagaiden <38038560+Armagaiden@users.noreply.github.com>
2021-04-30 07:36:09 +0200

    Add files via upload

---
+++ TP06/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TP06.Models
+{
+    public class Context : DbContext
+    {   
+        public Context() : base("name=Context")
+        {
+        }
+        public System.Data.Entity.DbSet<TP06_BO.Samourai> Samourais { get; set; }
+        public System.Data.Entity.DbSet<TP06_BO.Arme> Armes { get; set; }
+    }
+}
---
+++ TP06/Models/SamouraiVM.cs
@@ -0,0 +1,12 @@
+﻿using TP06_BO;
+using System.Collections.Generic;
+
+namespace TP06.Models
+{
+    public class SamouraiVM
+    {
+        public Samourai Samourai { get; set; }
+        public List<Arme> Armes { get; set; }
+        public int? IdSelectedArme { get; set; }
+    }
+}
